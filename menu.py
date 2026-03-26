2dd8e81dc665a9350e4c8a1646d5c6c0341c8efc
Kevin <kevinmercado1912000@icloud.com>
2026-03-26 09:28:41 -0500

    working...

--- menu.py
+++ menu.py
@@ -1,22 +1,16 @@
-database = []
+from add_product import add_product as adp
+from Show_inventory import show_inventory as show
+from calculate_total import calculate_total as Cal 
 m = True
 
-
-while m:
-
-    def menu ():
-    
+def menu ():
+     option = ""
+     while m:
         print('\n --- Menu Options ---\n')
         print('1.Add Products.')
         print('2. Show Inventory.')
         print('3.Calculate Total')
         print('4. Exit')
-        return menu
-    
-def menu():
-    
-        option = int(input('\n Please select a option \n'))
-        print('Invalid option')
 
-
-menu()
+        if option == 1:
+            print('Please enter a product: ')
