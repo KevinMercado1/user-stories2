db1bfa7d5a89c87f36041b7ec83aeafc7b9702a5
Kevin <kevinmercado1912000@icloud.com>
2026-03-26 09:08:48 -0500

    working...

---
+++ menu.py
@@ -0,0 +1,22 @@
+database = []
+m = True
+
+
+while m:
+
+    def menu ():
+    
+        print('\n --- Menu Options ---\n')
+        print('1.Add Products.')
+        print('2. Show Inventory.')
+        print('3.Calculate Total')
+        print('4. Exit')
+        return menu
+    
+def menu():
+    
+        option = int(input('\n Please select a option \n'))
+        print('Invalid option')
+
+
+menu()
